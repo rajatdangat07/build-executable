b21a749d88bd7aa5556928c26e101b4df117e3d8
rajatdangat07 <dangatrajat@gmail.com>
2022-01-07 12:23:55 +0530

    write code for build executable

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,86 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+import {promisify} from 'util';
+import * as core from '@actions/core';
+import clone from 'git-clone/promise';
+
+const exec = promisify(require('child_process').exec);
 
 async function run(): Promise<void> {
   try {
-    const nameToGreet = core.getInput('who-to-greet')
-    core.info(`Hello ${nameToGreet}`)
-    const time = new Date().toTimeString()
-    core.setOutput('time', time)
+    const pat = core.getInput('pat');
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    core.info(`The event payload: ${payload}`)
+    core.info('Frontend repository clone STARTED');
+
+    await clone(
+      `https://${pat}@github.com/thingsup/thingsup-tracking-frontend.git`,
+      './tmp/frontend'
+    );
+
+    core.info('Frontend repository clone DONE');
+
+    core.info('Executing npm build script in frontend repo');
+    {
+      const {stderr} = await exec(
+        'cd ./tmp/frontend && npm install && npm run build'
+      );
+
+      if (stderr && stderr instanceof Error) {
+        core.error('Executing npm build script in frontend repo FAILED');
+        core.setFailed(stderr);
+      }
+    }
+    core.info('Executing npm build script in frontend repo DONE');
+
+    core.info('Backend repository clone STARTED');
+
+    await clone(
+      `https://${pat}@github.com/thingsup/ColdChainServer.git`,
+      './tmp/backend'
+    );
+
+    core.info('Backend repository clone DONE');
+
+    core.info('Copy frontend build folder to backend STARTED');
+    {
+      const {stderr} = await exec(
+        'cp -frp ./tmp/frontend/dist -T ./tmp/backend/build'
+      );
+
+      if (stderr && stderr instanceof Error) {
+        core.error('Copy frontend build folder to backend FAILED');
+        core.setFailed(stderr);
+      }
+    }
+    core.info('Copy frontend build folder to backend DONE');
+
+    core.info('Install pkg library STARTED');
+    {
+      const {stderr} = await exec('npm i pkg -g');
+
+      if (stderr && stderr instanceof Error) {
+        core.error('Install pkg library FAILED');
+        core.setFailed(stderr);
+      }
+    }
+    core.info('Install pkg library DONE');
+
+    core.info('bundle code STARTED');
+    const version = '0.0.1';
+    const distFolderName = `dist_${version}`;
+    {
+      const {stderr} = await exec(
+        `cd ./tmp/backend && pkg --compress GZip --out-path ./${distFolderName} .`
+      );
+
+      if (stderr && stderr instanceof Error) {
+        core.error('bundle code FAILED');
+        core.setFailed(stderr);
+      }
+    }
+    core.info('bundle code DONE');
+    core.setOutput('distPath', `./tmp/backend/${distFolderName}`);
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-run()
+run();
